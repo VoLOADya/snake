c32bac7f3271199287cdc7220b48db9be6992776
VoLOADya <baron1983@list.ru>
2015-09-22 16:21:27 +0300

    Function

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,8 +10,26 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!!");
+            int x1 = 1;
+            int y1 = 3;
+            char sym1 = '*';
+            //     Console.SetCursorPosition(x1, y1);
+            //     Console.Write(sym1);
+            DrawSym(x1, y1, sym1);
+            
+            int x2 = 3;
+            int y2 = 5;
+            char sym2 = '#';
+            //      Console.SetCursorPosition(x2, y2);
+            //       Console.Write(sym2);
+            DrawSym(x2, y2, sym2);
+
             Console.ReadLine();
+        }
+            static void DrawSym(int x,int y,char sym)
+            {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
