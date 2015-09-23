0ca38ca037024548c45dfc07e4fc67de7a0f701d
VoLOADya <baron1983@list.ru>
2015-09-23 20:36:37 +0300

    Figure

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,20 +10,20 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-
+            Console.SetBufferSize(80,25);
             Point p1 = new Point(1,3,'*');
             p1.DrawSym();
 
             Point p2 = new Point(4,5,'*');
             p2.DrawSym();
 
-            Horizont hLine1 = new Horizont(0,25,0,'#');
+            Horizont hLine1 = new Horizont(0,78,0,'#');
             hLine1.Draw();
-            Horizont hLine2 = new Horizont(0, 25, 8, '#');
+            Horizont hLine2 = new Horizont(0, 78, 24, '#');
             hLine2.Draw();
-            Vertical vLine1 = new Vertical(0,0,8,'#');
+            Vertical vLine1 = new Vertical(0,0,24,'#');
             vLine1.Draw();
-            Vertical vLine2 = new Vertical(25, 0, 8, '#');
+            Vertical vLine2 = new Vertical(78, 0, 24, '#');
             vLine2.Draw();
 
 
