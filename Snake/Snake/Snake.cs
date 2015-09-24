61d7a542ad74badc79a28aea889dacaee94d8611
VoLOADya <baron1983@list.ru>
2015-09-24 21:37:15 +0300

    Real

--- Snake/Snake/Snake.cs
+++ Snake/Snake/Snake.cs
@@ -10,9 +10,9 @@ namespace Snake
     {
         public Snake(Point tail, int lenght, Direction direction)
         {
+            pList = new List<Point>();
             for (int i = 0; i < lenght; i++)
             {
-                pList = new List<Point>(); 
                 Point p = new Point(tail);
                 p.Move(i , direction);
                 pList.Add(p);
